d10dd9938144caeac84e455c1c515c2ae7b2673f
Kurt Eckhardt <kurte@rockisland.com>
2019-10-24 15:09:06 -0700

    T4 - Default allow faster speeds...
    I changed the Clock that was used and settings.
    So I think maximum speed changed from about 37+mhz to 60mhz
    
    I was haing issues at 60mhz with simple test so also changed the IO ins Speed and DSE values which appeared to work.
    
    I tried running our ILI9488_t3 library at 60mhz and it appears to work :D

--- SPI.cpp
+++ SPI.cpp
@@ -1278,9 +1278,11 @@ void SPIClass::begin()
 	hardware().clock_gate_register &= ~hardware().clock_gate_mask;
 
 	CCM_CBCMR = (CCM_CBCMR & ~(CCM_CBCMR_LPSPI_PODF_MASK | CCM_CBCMR_LPSPI_CLK_SEL_MASK)) |
-		CCM_CBCMR_LPSPI_PODF(6) | CCM_CBCMR_LPSPI_CLK_SEL(2); // pg 714
+		CCM_CBCMR_LPSPI_PODF(2) | CCM_CBCMR_LPSPI_CLK_SEL(1); // pg 714
+//		CCM_CBCMR_LPSPI_PODF(6) | CCM_CBCMR_LPSPI_CLK_SEL(2); // pg 714
 
-	uint32_t fastio = IOMUXC_PAD_DSE(6) | IOMUXC_PAD_SPEED(1);
+	uint32_t fastio = IOMUXC_PAD_DSE(7) | IOMUXC_PAD_SPEED(2);
+	//uint32_t fastio = IOMUXC_PAD_DSE(6) | IOMUXC_PAD_SPEED(1);
 	//uint32_t fastio = IOMUXC_PAD_DSE(3) | IOMUXC_PAD_SPEED(3);
 	//Serial.printf("SPI MISO: %d MOSI: %d, SCK: %d\n", hardware().miso_pin[miso_pin_index], hardware().mosi_pin[mosi_pin_index], hardware().sck_pin[sck_pin_index]);
 	*(portControlRegister(hardware().miso_pin[miso_pin_index])) = fastio;
